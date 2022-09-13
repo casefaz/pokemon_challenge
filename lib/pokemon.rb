b645a86a7398b3d83a4fc2e67f6a6fa410d3cff5
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-09-13 08:46:18 -0600

    Passing status tests until special effects

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -14,14 +14,23 @@ class Pokemon
 
   def apply(effect_name, round_applied_at)
     duration = STATUSES[effect_name][:duration]
-
-    round_applied_at.upto(duration) do |num|
-      @battle[num] = STATUSES[effect_name][:name]
+    # round = round_applied_at
+    round_applied_at.upto(duration + round_applied_at - 1) do |num|
+      if !STATUSES[@battle[num]].nil? && STATUSES[effect_name][:strength] >= STATUSES[@battle[num]][:strength]
+        @battle[num] = STATUSES[effect_name][:name]
+      elsif @battle[num].nil?
+        @battle[num] = STATUSES[effect_name][:name]
+      end
     end
-    binding.pry
+    # while round <= duration
+    #   @battle[round] = effect_name
+    #   round += 1
+    # end
+    # binding.pry 
   end
 
   def status_at(round)
+    # binding.pry
     @battle[round]
   end
 end
