43a17f3c7926487ef74363c5251bbc21225c8789
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-09-13 06:46:47 -0600

    Passing all sleep and nil tests

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -7,24 +7,21 @@ class Pokemon
     "confusion" => { name: "confusion", duration: 10, strength: 1 }
   }
   attr_reader :name
-  def initialize(name:)
-    @name = name
-    @battle = { 0 => nil }
+  def initialize(hash)
+    @name = hash[:name]
+    @battle = Hash.new(nil)
   end
 
   def apply(effect_name, round_applied_at)
+    duration = STATUSES[effect_name][:duration]
+
+    round_applied_at.upto(duration) do |num|
+      @battle[num] = STATUSES[effect_name][:name]
+    end
     binding.pry
-    # loop through battle, add number to round, at each round(#) add effect name and the round it was applied
-    # put it in an array and reduce it? - how would it store the data? increment the round and add the effect name for the duration?
-    # also consider linked lists and recursion in order to ADD and not just overwrite
-    # statuses[:duration].times do
-    @battle = { round_applied_at => effect_name }
-    # binding.pry
   end
 
   def status_at(round)
-    binding.pry
-    # deincrementation between duration and round happens in this method
     @battle[round]
   end
 end
